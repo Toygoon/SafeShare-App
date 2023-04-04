d722ea5f515905f583def83a140245f17ba7a6f0
Toygoon <toygoon@gmail.com>
2023-04-04 17:14:18 +0900

    [Add] 상수 클래스 추가

---
+++ app/src/main/java/com/toygoon/safeshare/Constants.java
@@ -0,0 +1,7 @@
+package com.toygoon.safeshare;
+
+public class Constants {
+    public static final String API_URL = "https://safeshare.toygoon.com/";
+    public static final String API_LOGIN_URL = API_URL + "login/";
+
+}
