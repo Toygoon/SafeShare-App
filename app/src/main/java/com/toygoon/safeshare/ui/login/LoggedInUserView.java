8994cc1fc203947868b5fb5b3194ef59c6463420
Toygoon <toygoon@gmail.com>
2023-04-10 21:28:37 +0900

    [Edit] LoggedInUserView에서 사용자 이름과 아이디를 저장하도록 설정

--- app/src/main/java/com/toygoon/safeshare/ui/login/LoggedInUserView.java
+++ app/src/main/java/com/toygoon/safeshare/ui/login/LoggedInUserView.java
@@ -4,14 +4,20 @@ package com.toygoon.safeshare.ui.login;
  * Class exposing authenticated user details to the UI.
  */
 class LoggedInUserView {
-    private String displayName;
+    private String userId;
+    private String name;
     //... other data fields that may be accessible to the UI
 
-    LoggedInUserView(String displayName) {
-        this.displayName = displayName;
+    LoggedInUserView(String userId, String name) {
+        this.userId = userId;
+        this.name = name;
     }
 
-    String getDisplayName() {
-        return displayName;
+    String getUserId() {
+        return this.userId;
+    }
+
+    String getName() {
+        return this.name;
     }
 }
