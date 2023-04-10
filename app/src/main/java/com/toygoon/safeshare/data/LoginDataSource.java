77a2cac26ef4e658bed5ae279e6a6c6af46f4344
Toygoon <toygoon@gmail.com>
2023-04-10 21:29:11 +0900

    [Edit] LoginDataSource에서 로그인 API를 이용한 POST 요청을 보내도록 수정

--- app/src/main/java/com/toygoon/safeshare/data/LoginDataSource.java
+++ app/src/main/java/com/toygoon/safeshare/data/LoginDataSource.java
@@ -1,8 +1,15 @@
 package com.toygoon.safeshare.data;
 
+import android.util.Log;
+
+import com.toygoon.safeshare.Constants;
 import com.toygoon.safeshare.data.model.LoggedInUser;
+import com.toygoon.safeshare.http.NetworkTask;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -10,17 +17,30 @@ import java.io.IOException;
 public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
+        // Login process
+        HashMap<String, String> map = new HashMap<>();
+        map.put("user_id", username);
+        map.put("user_pw", password);
+
+        NetworkTask task = new NetworkTask(Constants.API_LOGIN_URL, map, "POST");
+        CompletableFuture<HashMap<String, String>> future = CompletableFuture.supplyAsync(task);
+        HashMap<String, String> result = null;
 
         try {
-            // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
+            result = future.get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        // End login process
+
+        if (result.getOrDefault("response_code", "-1").equals(Constants.LOGIN_SUCCEED_CODE)) {
+            Log.d("LoginDataSource", "login succeed");
+
+            return new Result.Success<>(new LoggedInUser(result.get("name"), username));
+        }
+
+        Log.d("LoginDataSource", "Login failed");
+        return new Result.Error(new IOException("Error logging in", new Throwable("error")));
     }
 
     public void logout() {
