dce25931981576de2927138fe41010ed0e7b2a73
Toygoon <toygoon@gmail.com>
2023-04-10 21:27:23 +0900

    [Add] 로그인 성공시 받아올 response code를 Constants 클래스에 추가

--- app/src/main/java/com/toygoon/safeshare/Constants.java
+++ app/src/main/java/com/toygoon/safeshare/Constants.java
@@ -3,5 +3,5 @@ package com.toygoon.safeshare;
 public class Constants {
     public static final String API_URL = "https://safeshare.toygoon.com/api/";
     public static final String API_LOGIN_URL = API_URL + "login/";
-
+    public static final String LOGIN_SUCCEED_CODE = "200";
 }
