36c23d7d20c89fd33583de23910297048e6b0a8c
Toygoon <toygoon@gmail.com>
2023-05-03 23:50:50 +0900

    [Add] Constants 값으로 가입 API 주소 추가

--- app/src/main/java/com/toygoon/safeshare/Constants.java
+++ app/src/main/java/com/toygoon/safeshare/Constants.java
@@ -3,6 +3,7 @@ package com.toygoon.safeshare;
 public class Constants {
     public static final String API_URL = "https://safeshare.toygoon.com/api/";
     public static final String API_LOGIN_URL = API_URL + "login/";
+    public static final String API_REGISTER_URL = API_URL + "register/";
     public static final String LOGIN_SUCCEED_CODE = "200";
     public static final String LOGIN_PASSWORD_ERROR_CODE = "400";
     public static final String API_TOKEN_URL = API_URL + "app_token/";
