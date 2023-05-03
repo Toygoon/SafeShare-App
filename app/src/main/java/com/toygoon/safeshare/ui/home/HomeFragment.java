f8f6eea4948fd13a0ab2802944ea5b582b3c05d7
Toygoon <toygoon@gmail.com>
2023-05-04 00:08:46 +0900

    [Edit] 지도 위치가 현재 위치로 이동하도록 수정

--- app/src/main/java/com/toygoon/safeshare/ui/home/HomeFragment.java
+++ app/src/main/java/com/toygoon/safeshare/ui/home/HomeFragment.java
@@ -1,16 +1,25 @@
 package com.toygoon.safeshare.ui.home;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.toygoon.safeshare.databinding.FragmentHomeBinding;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class HomeFragment extends Fragment {
@@ -25,8 +34,36 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
         MapView mapView = new MapView(this.getContext());
+
+        // Move to current location
+        LocationManager lm = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+
+        // Request location permission start
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(requireActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+        // Request location permission end
+
+        Location userLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        MapPoint userMapPoint = MapPoint.mapPointWithGeoCoord(userLocation.getLatitude(), userLocation.getLongitude());
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("위치");
+        marker.setMapPoint(userMapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(marker);
+        mapView.setMapCenterPoint(userMapPoint, true);
         binding.mapView.addView(mapView);
 
         return root;
@@ -37,4 +74,5 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
