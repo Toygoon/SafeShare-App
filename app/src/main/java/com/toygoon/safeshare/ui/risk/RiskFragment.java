4174a3e22815bfc268ed835fdbb74475c60c0b86
Toygoon <toygoon@gmail.com>
2023-05-23 16:03:13 +0900

    [Add] 지도 받아오는 기능 구현 중, 구조 요청 샘플 텍스트 추가

--- app/src/main/java/com/toygoon/safeshare/ui/risk/RiskFragment.java
+++ app/src/main/java/com/toygoon/safeshare/ui/risk/RiskFragment.java
@@ -1,14 +1,24 @@
 package com.toygoon.safeshare.ui.risk;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -17,14 +27,17 @@ import com.toygoon.safeshare.R;
 import com.toygoon.safeshare.databinding.FragmentRiskBinding;
 import com.toygoon.safeshare.http.NetworkTask;
 
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
+
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 
-public class RiskFragment extends Fragment {
-
+public class RiskFragment extends Fragment implements MapReverseGeoCoder.ReverseGeoCodingResultListener {
     private FragmentRiskBinding binding;
+    private String userAddress;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,9 +48,11 @@ public class RiskFragment extends Fragment {
         View root = binding.getRoot();
 
         final Spinner riskSpinner = binding.riskFactorSpinner;
+        final EditText riskTitle = binding.riskFactorTitle, riskContent = binding.riskFactorContent;
+        final Button riskPhoto = binding.riskFactorPhoto;
         final String etc = getString(R.string.etc);
 
-        // Get pre-defined RiskFactors if available
+        // Get pre-defined RiskFactors if available Start
         NetworkTask task = new NetworkTask(Constants.API_RISK_FACTOR_URL, new HashMap<>(), "GET");
         CompletableFuture<HashMap<String, String>> future = CompletableFuture.supplyAsync(task);
         HashMap<String, String> result = null;
@@ -58,8 +73,53 @@ public class RiskFragment extends Fragment {
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_spinner_dropdown_item, riskFactors);
-        Log.d("GetRiskFactor", result.toString());
         riskSpinner.setAdapter(adapter);
+        // Get pre-defined RiskFactors if available End
+
+        // Get current location Start
+        LocationManager lm = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+
+        // Request location permission start
+        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getContext()),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(requireActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+        // Request location permission end
+
+        Location userLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        MapPoint userMapPoint = MapPoint.mapPointWithGeoCoord(userLocation.getLatitude(), userLocation.getLongitude());
+        MapReverseGeoCoder mapReverseGeoCoder = new MapReverseGeoCoder(getString(R.string.kakao_api), userMapPoint, this, requireActivity());
+
+        riskSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                while (true) {
+                    mapReverseGeoCoder.startFindingAddress();
+                    break;
+                }
+
+                if (riskSpinner.getSelectedItem().equals(getString(R.string.etc))) {
+                    riskTitle.setText("");
+                    riskContent.setText("");
+                } else {
+                    String current = riskSpinner.getSelectedItem().toString();
+                    riskTitle.setText(String.format(getString(R.string.risk_title_sample), current));
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         return root;
     }
@@ -68,5 +128,15 @@ public class RiskFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        this.userAddress = s;
+    }
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+        this.userAddress = "-1";
     }
 }
