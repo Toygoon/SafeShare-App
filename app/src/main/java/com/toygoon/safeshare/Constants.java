4bc67b9cb8e7b640d5dc5075029a443e0d3fe58a
Toygoon <toygoon@gmail.com>
2023-05-23 15:26:34 +0900

    [Add] RiskFactor를 받아오는 API 주소 추가, 받아오는 기능 구현

--- app/src/main/java/com/toygoon/safeshare/Constants.java
+++ app/src/main/java/com/toygoon/safeshare/Constants.java
@@ -4,7 +4,9 @@ public class Constants {
     public static final String API_URL = "https://safeshare.toygoon.com/api/";
     public static final String API_LOGIN_URL = API_URL + "login/";
     public static final String API_REGISTER_URL = API_URL + "register/";
+    public static final String API_TOKEN_URL = API_URL + "token/";
+    public static final String API_RISK_FACTOR_URL = API_URL + "get_risk_factor/";
+
     public static final String LOGIN_SUCCEED_CODE = "200";
     public static final String LOGIN_PASSWORD_ERROR_CODE = "400";
-    public static final String API_TOKEN_URL = API_URL + "token/";
 }
