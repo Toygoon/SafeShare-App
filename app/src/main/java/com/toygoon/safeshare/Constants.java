fcb822aec98ddaead2fe4c883a83b0d9ec3ece96
Toygoon <toygoon@gmail.com>
2023-06-04 16:36:19 +0900

    [Add] 구조 요청 API 주소 추가

--- app/src/main/java/com/toygoon/safeshare/Constants.java
+++ app/src/main/java/com/toygoon/safeshare/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static final String API_REGISTER_URL = API_URL + "register/";
     public static final String API_TOKEN_URL = API_URL + "token/";
     public static final String API_RISK_FACTOR_URL = API_URL + "get_risk_factor/";
+    public static final String API_RISK_REPORT_URL = API_URL + "risk_report/";
 
     public static final String LOGIN_SUCCEED_CODE = "200";
     public static final String LOGIN_PASSWORD_ERROR_CODE = "400";
